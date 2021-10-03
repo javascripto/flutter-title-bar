9be5cede35d02beb911ecf1672aaac0bb7d6e588
Yuri Almeida <yurialves2@gmail.com>
2021-10-03 16:37:20 -0300

    chore: set initial window size and minimum size

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() {
   runApp(MyApp());
+  doWhenWindowReady(() {
+    final initialSize = Size(800, 600);
+    appWindow.size = initialSize;
+    appWindow.minSize = initialSize;
+  });
 }
 
 class MyApp extends StatelessWidget {
